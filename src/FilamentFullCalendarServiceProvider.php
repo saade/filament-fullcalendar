e24cfeb2f55f68acfc2c9261fed70c7976ff766e
saade <saade@users.noreply.github.com>
2022-01-27 01:05:26 +0000

    Fix styling

--- src/FilamentFullCalendarServiceProvider.php
+++ src/FilamentFullCalendarServiceProvider.php
@@ -2,18 +2,18 @@
 
 namespace Saade\FilamentFullCalendar;
 
+use Saade\FilamentFullCalendar\Commands\UpgradeFilamentFullCalendarCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Saade\FilamentFullCalendar\Commands\UpgradeFilamentFullCalendarCommand;
 
 class FilamentFullCalendarServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
-    {        
+    {
         $package
             ->name('filament-fullcalendar')
             ->hasConfigFile()
-            ->hasViews()            
+            ->hasViews()
             ->hasCommand(UpgradeFilamentFullCalendarCommand::class);
     }
 }
