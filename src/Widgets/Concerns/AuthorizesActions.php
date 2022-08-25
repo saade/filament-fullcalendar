48e6ffd8118f3086747c83adc07ac20247d3e0d4
Tiago Fernandes <tiagoff@gmail.com>
2022-08-25 19:50:27 +0100

    Add View Modal and corresponding canView authorization.

--- src/Widgets/Concerns/AuthorizesActions.php
+++ src/Widgets/Concerns/AuthorizesActions.php
@@ -4,6 +4,16 @@ namespace Saade\FilamentFullCalendar\Widgets\Concerns;
 
 trait AuthorizesActions
 {
+    public static function canView(?array $event = null): bool
+    {
+        // If we want to prevent breaking changes, we need to "fallback"
+        // to "canEdit(Event)". Not doing that since the new behaviour
+        // is likely the desired one. The fallback would be:
+        // return static::canEdit($event);
+
+        return true;
+    }
+
     public static function canCreate(): bool
     {
         return true;
