fd35a1fc6518eac5cecae30db1413e3f507d5eb6
Guilherme Saade <saade@outlook.com.br>
2022-09-18 18:23:33 -0300

    Update resources/views/components/create-event-modal.blade.php

--- resources/views/components/create-event-modal.blade.php
+++ resources/views/components/create-event-modal.blade.php
@@ -2,7 +2,7 @@
     <x-filament::modal id="fullcalendar--create-event-modal" :width="$this->getModalWidth()">
         <x-slot name="header">
             <x-filament::modal.heading>
-                {{ $this->getCreateModalTitle() }}
+                {{ $this->getCreateEventModalTitle() }}
             </x-filament::modal.heading>
         </x-slot>
 
