b45f7feb978f6fda76fc3d77fd1bfbecb5729d65
Saade <saade@outlook.com.br>
2023-07-05 14:13:10 -0300

    add esbuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 const colors = require('tailwindcss/colors')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit',
-
-    content: [
-        './resources/views/**/*.blade.php',
-    ],
-
+    content: ['./resources/views/**/*.blade.php', './src/**/*.php'],
+    darkMode: 'class',
     theme: {
         extend: {
             colors: {
-                primary: colors.orange,
+                danger: colors.rose,
+                primary: colors.amber,
+                success: colors.green,
+                warning: colors.amber,
             },
         },
     },
-};
+    corePlugins: {
+        preflight: false,
+    },
+    plugins: [],
+}
