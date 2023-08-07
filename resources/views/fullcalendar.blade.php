b81676929062a299f4ea56c9a9f13c802d868b5c
alan <alan@solutionforest.net>
2023-08-07 13:49:35 +0800

    wip: support filamentphp v3

--- resources/views/fullcalendar.blade.php
+++ resources/views/fullcalendar.blade.php
@@ -1,84 +1,80 @@
 @php($locale = strtolower(str_replace('_', '-', $this->config('locale', config('app.locale')))))
 
-<x-filament::widget>
+<x-filament-widgets::widget>
     <x-filament::card>
-        <div
-            wire:ignore
-            x-ref="calendar"
-            x-data="calendarComponent({
-                key: @js($this->getKey()),
-                config: {{ json_encode($this->getConfig(), JSON_PRETTY_PRINT) }},
-                locale: '{{ $locale }}',
-                events: {{ json_encode($events) }},
-                initialView: @js($this->config('initialView')),
-                initialDate: @js($this->config('initialDate')),
-                shouldSaveState: @js($this->config('saveState', false)),
-                handleEventClickUsing: async ({ event, jsEvent }) => {
-                    if( event.url ) {
-                        jsEvent.preventDefault();
-                        window.open(event.url, event.extendedProps.shouldOpenInNewTab ? '_blank' : '_self');
-                        return false;
-                    }
-
-                    @if ($this::isListeningClickEvent())
-                        $wire.onEventClick(event)
-                    @endif
-                },
-                handleEventDropUsing: async ({ event, oldEvent, relatedEvents }) => {
-                    @if($this::isListeningDropEvent())
-                        $wire.onEventDrop(event, oldEvent, relatedEvents)
-                    @endif
-                },
-                handleEventResizeUsing: async ({ event, oldEvent, relatedEvents }) => {
-                    @if($this::isListeningResizeEvent())
-                        $wire.onEventResize(event, oldEvent, relatedEvents)
-                    @endif
-                },
-                handleDateClickUsing: async ({ date, allDay }) => {
-                    @if($this::canCreate())
-                        $wire.onCreateEventClick({ date, allDay })
-                    @endif
-                },
-                handleSelectUsing: async ({ start, end, allDay }) => {
-                    @if($this->config('selectable', false))
-                        $wire.onCreateEventClick({ start, end, allDay })
-                    @endif
-                },
-                fetchEventsUsing: async ({ start, end, allDay }, successCallback, failureCallback) => {
-                    @if( $this::canFetchEvents() )
-                        return $wire.fetchEvents({ start, end, allDay })
-                            .then(events => {
-                                if(events.length == 0) return Object.values($data.cachedEvents)
-                                if(events[0].id) {
-                                    events.forEach((event) => $data.cachedEvents[event.id] = event)
-                                    successCallback(Object.values($data.cachedEvents))
-                                } else{
-                                    successCallback(events)
-                                }
-                            })
-                            .catch( failureCallback );
-                    @else
-                        return successCallback([]);
-                    @endif
-                },
-            })"
-            x-on:filament-fullcalendar--refresh.window="
-                @if( $this::canFetchEvents() )
-                    $data.calendar.refetchEvents();
-                @else
-                    $data.calendar.removeAllEvents();
-                    event.detail.data.map(event => $data.calendar.addEvent(event));
+        <div wire:ignore x-ref="calendar" x-data="calendarComponent({
+            key: @js($this->getKey()),
+            config: {{ json_encode($this->getConfig(), JSON_PRETTY_PRINT) }},
+            locale: '{{ $locale }}',
+            events: {{ json_encode($events) }},
+            initialView: @js($this->config('initialView')),
+            initialDate: @js($this->config('initialDate')),
+            shouldSaveState: @js($this->config('saveState', false)),
+            handleEventClickUsing: async ({ event, jsEvent }) => {
+                if (event.url) {
+                    jsEvent.preventDefault();
+                    window.open(event.url, event.extendedProps.shouldOpenInNewTab ? '_blank' : '_self');
+                    return false;
+                }
+        
+                @if($this::isListeningClickEvent())
+                $wire.onEventClick(event)
                 @endif
+            },
+            handleEventDropUsing: async ({ event, oldEvent, relatedEvents }) => {
+                @if($this::isListeningDropEvent())
+                $wire.onEventDrop(event, oldEvent, relatedEvents)
+                @endif
+            },
+            handleEventResizeUsing: async ({ event, oldEvent, relatedEvents }) => {
+                @if($this::isListeningResizeEvent())
+                $wire.onEventResize(event, oldEvent, relatedEvents)
+                @endif
+            },
+            handleDateClickUsing: async ({ date, allDay }) => {
+                @if($this::canCreate())
+                $wire.onCreateEventClick({ date, allDay })
+                @endif
+            },
+            handleSelectUsing: async ({ start, end, allDay }) => {
+                @if($this->config('selectable', false))
+                $wire.onCreateEventClick({ start, end, allDay })
+                @endif
+            },
+            fetchEventsUsing: async ({ start, end, allDay }, successCallback, failureCallback) => {
+                @if($this::canFetchEvents())
+                return $wire.fetchEvents({ start, end, allDay })
+                    .then(events => {
+                        if (events.length == 0) return Object.values($data.cachedEvents)
+                        if (events[0].id) {
+                            events.forEach((event) => $data.cachedEvents[event.id] = event)
+                            successCallback(Object.values($data.cachedEvents))
+                        } else {
+                            successCallback(events)
+                        }
+                    })
+                    .catch(failureCallback);
+                @else
+                return successCallback([]);
+                @endif
+            },
+        })"
+            x-on:filament-fullcalendar--refresh.window="
+                @if ($this::canFetchEvents())
+$data.calendar.refetchEvents();
+@else
+$data.calendar.removeAllEvents();
+                    event.detail.data.map(event => $data.calendar.addEvent(event));
+@endif
             "
-            class="filament-fullcalendar--calendar"
-        ></div>
+            class="filament-fullcalendar--calendar"></div>
     </x-filament::card>
 
-    @if($this::canCreate())
+    @if ($this::canCreate())
         <x:filament-fullcalendar::create-event-modal />
     @endif
 
-    @if($this::canView())
+    @if ($this::canView())
         <x:filament-fullcalendar::edit-event-modal />
     @endif
-</x-filament::widget>
+</x-filament-widgets::widget>
--- src/FilamentFullCalendarServiceProvider.php
+++ src/FilamentFullCalendarServiceProvider.php
@@ -2,24 +2,29 @@
 
 namespace Saade\FilamentFullCalendar;
 
-use Filament\PluginServiceProvider;
+use Filament\Support\Facades\FilamentAsset;
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
 use Saade\FilamentFullCalendar\Commands\UpgradeFilamentFullCalendarCommand;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentFullCalendarServiceProvider extends PluginServiceProvider
+class FilamentFullCalendarServiceProvider extends PackageServiceProvider
 {
-    protected array $beforeCoreScripts = [
-        'filament-fullcalendar-scripts' => __DIR__ . '/../dist/filament-fullcalendar.js',
-    ];
+    public static string $name = 'filament-fullcalendar';
 
-    protected array $styles = [
-        'filament-fullcalendar-styles' => __DIR__ . '/../dist/filament-fullcalendar.css',
-    ];
+    public function packageBooted(): void
+    {
+        FilamentAsset::register([
+            Css::make('filament-fullcalendar-styles', __DIR__ . '/../dist/filament-fullcalendar.css'),
+            Js::make('filament-fullcalendar-scripts', __DIR__ . '/../dist/filament-fullcalendar.js'),
+        ], 'saade/filament-fullcalendar');
+    }
 
     public function configurePackage(Package $package): void
     {
         $package
-            ->name('filament-fullcalendar')
+            ->name(self::$name)
             ->hasConfigFile()
             ->hasViews()
             ->hasCommand(UpgradeFilamentFullCalendarCommand::class);
