cc052263ae856fe6d14205e063589711b56d3327
Tieme Alberts <52133032+sitenzo@users.noreply.github.com>
2024-03-02 11:29:44 +0100

    Update src/Widgets/Concerns/InteractsWithConfig.php
    Co-authored-by: Guilherme Saade <saade@outlook.com.br>

--- src/Widgets/Concerns/InteractsWithConfig.php
+++ src/Widgets/Concerns/InteractsWithConfig.php
@@ -11,18 +11,6 @@ trait CanBeConfigured
 		return [];
 	}
 
-	private static function mergeArraysRecursive($initial, $custom) {
-		foreach ($custom as $key => $value) {
-			if (is_array($value) && isset($initial[$key]) && is_array($initial[$key])) {
-				// If both are arrays, recursively merge them
-				$initial[$key] = mergeArraysRecursive($initial[$key], $value);
-			} else {
-				// Otherwise, overwrite the value
-				$initial[$key] = $value;
-			}
-		}
-		return $initial;
-	}
 
 
 	  protected function getConfig(): array
