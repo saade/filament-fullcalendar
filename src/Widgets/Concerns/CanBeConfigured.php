3d7d7a5b3f9e5b84423dc899e451a1be66f71ee0
Saade <saade@outlook.com.br>
2024-03-26 13:58:34 -0300

    wip

--- src/Widgets/Concerns/CanBeConfigured.php
+++ src/Widgets/Concerns/CanBeConfigured.php
@@ -4,6 +4,8 @@ namespace Saade\FilamentFullCalendar\Widgets\Concerns;
 
 use Saade\FilamentFullCalendar\FilamentFullCalendarPlugin;
 
+use function Saade\FilamentFullCalendar\array_merge_recursive_unique;
+
 trait CanBeConfigured
 {  
 	public function config(): array
@@ -11,11 +13,9 @@ trait CanBeConfigured
 		return [];
 	}
 
-
-
     protected function getConfig(): array
     {
-        return array_merge_recursive(
+        return array_merge_recursive_unique(
             FilamentFullCalendarPlugin::get()->getConfig(),
             $this->config(),
         );
