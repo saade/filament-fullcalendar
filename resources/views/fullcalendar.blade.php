73b440badfe4dbfbd21aacc43fd8113bdfcbed76
mario <mario.gattolla@gmail.com>
2025-09-16 16:02:34 +0200

    filament 4 support

--- resources/views/fullcalendar.blade.php
+++ resources/views/fullcalendar.blade.php
@@ -10,7 +10,6 @@
 
         <div class="filament-fullcalendar" wire:ignore x-load
             x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-fullcalendar-alpine', 'saade/filament-fullcalendar') }}"
-            ax-load-css="{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-fullcalendar-styles', 'saade/filament-fullcalendar') }}"
             x-ignore x-data="fullcalendar({
                 locale: @js($plugin->getLocale()),
                 plugins: @js($plugin->getPlugins()),
