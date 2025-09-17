05f3df1d8adb274dea425041e602898dfb1505e0
mario <mario.gattolla@gmail.com>
2025-09-17 11:40:36 +0200

    filament 4 support

--- src/Actions/ViewAction.php
+++ src/Actions/ViewAction.php
@@ -25,7 +25,7 @@ class ViewAction extends BaseViewAction
 
         $this->modalFooterActions(
             fn (ViewAction $action, FullCalendarWidget $livewire) => [
-                ...$livewire->getCachedModalActions(),
+                ...$livewire->getCachedFormActions(),
                 $action->getModalCancelAction(),
             ]
         );
